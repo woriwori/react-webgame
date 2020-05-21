dc9edb0818a92b73c67fa176799dd4524a72492f
[3기]이원희 <ireba4478@gmail.com>
2020-05-21 23:26:41 +0900

    2-5. babel 설치

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,28 @@ module.exports = {
   mode: 'development', // 배포용이면 production
   devtool: 'eval',
   resolve: {
-    extensions: ['jsx', 'js'],
+    extensions: ['.jsx', '.js'],
     // 이걸 하면, entry의 app에 파일 이름만 넣으면 이름.jsx, 이름.js를 다 찾아서 합쳐준다.
   },
   entry: {
     // 입력 (최고 중요)
     app: ['./client'], // WordRelay는 client.jsx안에서 불러와주고 있음.
   },
+
+  // entry에 module을 적용해서 output으로 뺀다.
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, // jsx 를
+        loader: 'babel-loader', // babel loader로 돌리겟다
+        options: {
+          // 이 옵션을 사용해서
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+
   output: {
     // 출력 (최고 중요)
     path: path.join(__dirname, 'dist'), // __dirname이 현재 프로젝트 폴더.(dirname: C:/user/....)
