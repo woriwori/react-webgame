94c8efc7ca4c52f188ef81edc91018bb44bfd3ee
이원희 <wonhee_lee@tmax.co.kr>
2020-05-25 17:45:38 +0900

    2-9. hot loader 설치 및 설정

--- client.jsx
+++ client.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import WordRelay from './WordRelay';
+import { hot } from 'react-hot-loader/root';
 
-render(<WordRelay />, document.getElementById('root'));
+const Hot = hot(WordRelay);
+
+render(<Hot />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
             ],
             '@babel/preset-react',
           ],
-          plugins: ['@babel/plugin-proposal-class-properties'],
+          plugins: ['@babel/plugin-proposal-class-properties', 'react-hot-loader/babel'],
         },
       },
     ],
