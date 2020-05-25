5d3dba038f5809891e6392f6e14e7a31f55922a5
이원희 <wonhee_lee@tmax.co.kr>
2020-05-25 16:57:04 +0900

    2-8. 끝말잇기 코드 추가

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
 
 class WordRelay extends Component {
+  state = {
+    word: '제로초',
+    value: '',
+    result: '',
+  };
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log(this.state.word[this.state.word.length - 1]);
+    console.log(this.state.value[0]);
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState((prevState) => ({
+        result: '딩동댕',
+        word: prevState.value,
+        value: '',
+      }));
+    } else {
+      this.setState({
+        ...this.state,
+        result: '땡',
+        value: '',
+      });
+    }
+  };
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+  input;
+  onRefInput = (c) => {
+    this.input = c;
+  };
   render() {
-    return <div></div>;
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input type="text" ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+          <button>입력!</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
+    );
   }
 }
 
