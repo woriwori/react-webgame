69ae250edd6c045751b1cfe49f91c6ca4c0be44d
이원희 <wonhee_lee@tmax.co.kr>
2020-05-25 18:19:30 +0900

    2-10. hooks 로 변경

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -1,48 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useRef } from 'react';
 
-class WordRelay extends Component {
-  state = {
-    word: '제로초',
-    value: '',
-    result: '',
-  };
-  onSubmitForm = (e) => {
+const WordRelay = () => {
+  const [word, setWord] = useState('제로초');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputRef = useRef(null);
+  const onSubmitForm = (e) => {
     e.preventDefault();
-    console.log(this.state.word[this.state.word.length - 1]);
-    console.log(this.state.value[0]);
-    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-      this.setState((prevState) => ({
-        result: '딩동댕',
-        word: prevState.value,
-        value: '',
-      }));
+    if (word[word.length - 1] === value[0]) {
+      setResult('딩동댕');
+      setWord(value);
+      setValue('');
     } else {
-      this.setState({
-        ...this.state,
-        result: '땡',
-        value: '',
-      });
+      setResult('땡');
+      setValue('');
     }
+    inputRef.current.focus();
   };
-  onChangeInput = (e) => {
-    this.setState({ value: e.target.value });
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
   };
-  input;
-  onRefInput = (c) => {
-    this.input = c;
-  };
-  render() {
-    return (
-      <>
-        <div>{this.state.word}</div>
-        <form onSubmit={this.onSubmitForm}>
-          <input type="text" ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
-          <button>입력!</button>
-        </form>
-        <div>{this.state.result}</div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input type="text" ref={inputRef} value={value} onChange={onChangeInput} />
+        <button>입력!</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
 
 export default WordRelay;
