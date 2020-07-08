6dd97b5560d55fc590557844582a6dd891301314
이원희 <wonhee_lee@tmax.co.kr>
2020-07-08 17:16:44 +0900

    가위바위보 게임 구현

--- game5/RSC.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class RSC extends Component {
-  render() {
-    return <div>ddd</div>;
-  }
-}
-
-export default RSC;
--- game5/client.jsx
+++ game5/client.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import RSC from './RSC';
+import RSP from './RSP';
 import { hot } from 'react-hot-loader/root';
 
-const Hot = hot(RSC);
+const Hot = hot(RSP);
 
 render(<Hot />, document.getElementById('root'));
